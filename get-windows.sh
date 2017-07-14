0066706b8a76c72de19edeaa19a8906dc8fd63ef
ia <isaac.ardis@gmail.com>
2017-07-14 15:47:25 -0500

    problem: use isaac fork for tests and fix win extension
    soltuion: do it

--- get-windows.sh
+++ get-windows.sh
@@ -8,13 +8,18 @@
 # Install janus executable to system PATH.
 #
 # curl -sL https://raw.githubusercontent.com/ethereumproject/janus/master/get.sh | bash
+# export PATH=$PATH:$PWD/janusbin
 
-set /p TAR_FILE="/tmp/janus.zip"
-set /p TAR_FILE_SIG="/tmp/janus.zip.sig"
-set /p ISAAC_GPG_FILE="/tmp/isaac-gpg.txt"
-set /p ISAAC_GPG_URL="https://raw.githubusercontent.com/ethereumproject/volunteer/master/Volunteer-Public-Keys/isaac.ardis%40gmail.com"
-set /p DOWNLOAD_URL="https://github.com/whilei/janus/releases/download"
-test -z "%TMPDIR%" && set /p TMPDIR="$(mktemp -d)"
+set -e
+
+TAR_FILE="/tmp/janus.tar.gz"
+TAR_FILE_SIG="/tmp/janus.tar.gz.sig"
+ISAAC_GPG_FILE="/tmp/isaac-gpg.txt"
+ISAAC_GPG_URL="https://raw.githubusercontent.com/ethereumproject/volunteer/master/Volunteer-Public-Keys/isaac.ardis%40gmail.com"
+DOWNLOAD_URL="https://github.com/whilei/janus/releases/download"
+test -z "$TMPDIR" && TMPDIR="$(mktemp -d)"
+
+WINDOWS_EXT="Windows_x86_64.zip"
 
 last_version() {
   # The new and improved sans-GithubAPI-rate-limited curler.
@@ -23,31 +28,31 @@ last_version() {
 }
 
 download() {
-  test -z "%VERSION%" && set /p VERSION="$(last_version)"
-  test -z "%VERSION%" && {
+  test -z "$VERSION" && VERSION="$(last_version)"
+  test -z "$VERSION" && {
     echo "Unable to get janus version." >&2
     exit 1
   }
-  echo "Version: %VERSION%"
-  rm -f "%TAR_FILE%"
-  download_target="%DOWNLOAD_URL%/%VERSION%/janus_%VERSION%_$(uname -s)_$(uname -m).zip"
-  echo "Downloading Janus: %download_target%"
-  curl -s -L -o "%TAR_FILE%" \
-    "%download_target%"
+  echo "Version: $VERSION"
+  rm -f "$TAR_FILE"
+  download_target="$DOWNLOAD_URL/$VERSION/janus_${VERSION}_$WINDOWS_EXT"
+  echo "Downloading Janus: $download_target"
+  curl -s -L -o "$TAR_FILE" \
+    "$download_target"
 
   # Get and verify signature.
-  rm -f "%TAR_FILE_SIG%"
-  sig_target="%DOWNLOAD_URL%/%VERSION%/janus_%VERSION%_$(uname -s)_$(uname -m).zip.sig"
-  echo "Downloading signature: %sig_target%"
-  curl -s -L -o "%TAR_FILE_SIG%" \
-    "%sig_target%"
+  rm -f "$TAR_FILE_SIG"
+  sig_target="$DOWNLOAD_URL/$VERSION/janus_${VERSION}_$WINDOWS_EXT.sig"
+  echo "Downloading signature: $sig_target"
+  curl -s -L -o "$TAR_FILE_SIG" \
+    "$sig_target"
 
   # Ensure our newly downloaded files exists.
-  if ! [ -f "%TAR_FILE%" ]; then
+  if ! [ -f "$TAR_FILE" ]; then
           echo "Tar file not found."
           exit 1
   fi
-  if ! [ -f "%TAR_FILE_SIG%" ]; then
+  if ! [ -f "$TAR_FILE_SIG" ]; then
           echo "Tar sig file not found."
           exit 1
   fi
@@ -56,24 +61,28 @@ download() {
 verify() {
   # Verify signature if gpg is available.
   if command -v gpg 2> /dev/null; then
-          echo "Downloading Isaac's key file: %ISAAC_GPG_URL%"
-          curl -s -L -o "%ISAAC_GPG_FILE%" \
-            "%ISAAC_GPG_URL%"
+          echo "Downloading Isaac's key file: $ISAAC_GPG_URL"
+          curl -s -L -o "$ISAAC_GPG_FILE" \
+            "$ISAAC_GPG_URL"
 
-          gpg --import "%ISAAC_GPG_FILE%"
-          gpg --verify "%TAR_FILE_SIG%" "%TAR_FILE%"
+          gpg --import "$ISAAC_GPG_FILE"
+          gpg --verify "$TAR_FILE_SIG" "$TAR_FILE"
   fi
 }
 
 install() {
-  tar -xf "%TAR_FILE%" -C "%TMPDIR%"
-  # Ensure executable
-  # Add to PATH
-  # set PATH=%PATH%;"${TMPDIR}/janus"
+  echo "Extracting janus..."
+  tar -xf "$TAR_FILE"
+
+  echo "Moving 'janus' binary to ./janusbin/janus"
+  mkdir janusbin
+  mv janus janusbin/
+
+  echo "Please ensure janus is added to PATH."
+  echo "Use:"
+  echo " - export PATH=$PATH:$PWD/janusbin"
 }
 
 download
 verify
 install
-
-echo "Janus installed to: $(which janus)"
