3c3998f9c4e0dcc14918f51e17d1710423819f44
ia <isaac.ardis@gmail.com>
2017-07-14 15:28:11 -0500

    problem: installer for windows
    solution: change vars to use % instead of $

--- get-windows.sh
+++ get-windows.sh
@@ -9,47 +9,45 @@
 #
 # curl -sL https://raw.githubusercontent.com/ethereumproject/janus/master/get.sh | bash
 
-set -e
-
-TAR_FILE="/tmp/janus.zip"
-TAR_FILE_SIG="/tmp/janus.zip.sig"
-ISAAC_GPG_FILE="/tmp/isaac-gpg.txt"
-ISAAC_GPG_URL="https://raw.githubusercontent.com/ethereumproject/volunteer/master/Volunteer-Public-Keys/isaac.ardis%40gmail.com"
-DOWNLOAD_URL="https://github.com/ethereumproject/janus/releases/download"
-test -z "$TMPDIR" && TMPDIR="$(mktemp -d)"
+set /p TAR_FILE="/tmp/janus.zip"
+set /p TAR_FILE_SIG="/tmp/janus.zip.sig"
+set /p ISAAC_GPG_FILE="/tmp/isaac-gpg.txt"
+set /p ISAAC_GPG_URL="https://raw.githubusercontent.com/ethereumproject/volunteer/master/Volunteer-Public-Keys/isaac.ardis%40gmail.com"
+set /p DOWNLOAD_URL="https://github.com/whilei/janus/releases/download"
+test -z "%TMPDIR%" && set /p TMPDIR="$(mktemp -d)"
 
 last_version() {
   # The new and improved sans-GithubAPI-rate-limited curler.
   # https://github.com/goreleaser/goreleaser/issues/157
-  curl -sL -o /dev/null -w %{url_effective} https://github.com/ethereumproject/janus/releases/latest | rev | cut -f1 -d'/'| rev
+  curl -sL -o /dev/null -w %{url_effective} https://github.com/whilei/janus/releases/latest | rev | cut -f1 -d'/'| rev
 }
 
 download() {
-  test -z "$VERSION" && VERSION="$(last_version)"
-  test -z "$VERSION" && {
+  test -z "%VERSION%" && set /p VERSION="$(last_version)"
+  test -z "%VERSION%" && {
     echo "Unable to get janus version." >&2
     exit 1
   }
-  echo "Version: $VERSION"
-  rm -f "$TAR_FILE"
-  download_target="$DOWNLOAD_URL/$VERSION/janus_${VERSION}_$(uname -s)_$(uname -m).zip"
-  echo "Downloading Janus: $download_target"
-  curl -s -L -o "$TAR_FILE" \
-    "$download_target"
+  echo "Version: %VERSION%"
+  rm -f "%TAR_FILE%"
+  download_target="%DOWNLOAD_URL%/%VERSION%/janus_%VERSION%_$(uname -s)_$(uname -m).zip"
+  echo "Downloading Janus: %download_target%"
+  curl -s -L -o "%TAR_FILE%" \
+    "%download_target%"
 
   # Get and verify signature.
-  rm -f "$TAR_FILE_SIG"
-  sig_target="$DOWNLOAD_URL/$VERSION/janus_${VERSION}_$(uname -s)_$(uname -m).zip.sig"
-  echo "Downloading signature: $sig_target"
-  curl -s -L -o "$TAR_FILE_SIG" \
-    "$sig_target"
+  rm -f "%TAR_FILE_SIG%"
+  sig_target="%DOWNLOAD_URL%/%VERSION%/janus_%VERSION%_$(uname -s)_$(uname -m).zip.sig"
+  echo "Downloading signature: %sig_target%"
+  curl -s -L -o "%TAR_FILE_SIG%" \
+    "%sig_target%"
 
   # Ensure our newly downloaded files exists.
-  if ! [ -f "$TAR_FILE" ]; then
+  if ! [ -f "%TAR_FILE%" ]; then
           echo "Tar file not found."
           exit 1
   fi
-  if ! [ -f "$TAR_FILE_SIG" ]; then
+  if ! [ -f "%TAR_FILE_SIG%" ]; then
           echo "Tar sig file not found."
           exit 1
   fi
@@ -58,21 +56,20 @@ download() {
 verify() {
   # Verify signature if gpg is available.
   if command -v gpg 2> /dev/null; then
-          echo "Downloading Isaac's key file: $ISAAC_GPG_URL"
-          curl -s -L -o "$ISAAC_GPG_FILE" \
-            "$ISAAC_GPG_URL"
+          echo "Downloading Isaac's key file: %ISAAC_GPG_URL%"
+          curl -s -L -o "%ISAAC_GPG_FILE%" \
+            "%ISAAC_GPG_URL%"
 
-          gpg --import "$ISAAC_GPG_FILE"
-          gpg --verify "$TAR_FILE_SIG" "$TAR_FILE"
+          gpg --import "%ISAAC_GPG_FILE%"
+          gpg --verify "%TAR_FILE_SIG%" "%TAR_FILE%"
   fi
 }
 
 install() {
-  tar -xf "$TAR_FILE" -C "$TMPDIR"
+  tar -xf "%TAR_FILE%" -C "%TMPDIR%"
   # Ensure executable
-  chmod +x "${TMPDIR}/janus"
   # Add to PATH
-  set PATH=%PATH%;"${TMPDIR}/janus"
+  # set PATH=%PATH%;"${TMPDIR}/janus"
 }
 
 download
